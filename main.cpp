11d62323bfea1dabbc850855cd08dc34b518ae03
Ali Rouhi <thealirouhi@gmail.com>
2026-05-15 14:49:50 +0330

    Implement isValid function to check number placement in Sudoku

--- main.cpp
+++ main.cpp
@@ -33,6 +33,31 @@ void printBoard(int board[9][9]) {
     }
 }
 
+bool isValid(int board[9][9], int row, int col, int num) {
+    // check row
+    for (int j = 0; j < 9; j++) {
+        if (board[row][j] == num) return false;
+    }
+
+    // check column
+    for (int i = 0; i < 9; i++) {
+        if (board[i][col] == num) return false;
+    }
+
+    // check 3x3 box
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[startRow + i][startCol + j] == num)
+                return false;
+        }
+    }
+
+    return true;
+}
+
 bool solve(int board[9][9]) {
     return true;
 }
